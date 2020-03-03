98e66abd393618746b6b07b14a6a52d2b2bcc7a2
S.Pragashraj <rajpragashraj30@gmail.com>
2020-03-03 07:32:40 +0530

    setup

---
+++ Sliding Game/Assets/Scripts/level3Quit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class level3Quit : MonoBehaviour {
+	
+	public GameObject completeLevelUI;
+
+	public void OnTriggerEnter(Collider collider){
+		    completeLevelUI.SetActive(true);
+			 StartCoroutine(finishing());
+	}
+
+	IEnumerator finishing(){
+		yield return new WaitForSeconds(3);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+	}
+}
